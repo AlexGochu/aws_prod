e2ce17cff26678906b8867924b4366a8bc0b70c0
GoaL <12134884+AlexGochu@users.noreply.github.com>
2021-12-25 00:44:40 +0000

    changes texts

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ st.title('AWS Cifar10')
 image_file = st.file_uploader('Load an image', type=['png', 'jpg'])  # Добавление загрузчика файлов
 
 if not image_file is None:                                           # Выполнение блока, если загружено изображение
-    col = st.beta_columns(1)
+    col1, col2 = st.beta_columns(2)
     image = Image.open(image_file)                                   # Открытие изображения
     pred_result = process(image_file)                                    # Обработка изображения с помощью функции, реализованной в другом файле
     
     
     result = '<p style="color: blue; font-size: 42px;">'+pred_result+'</p>'
-    col.text(st.markdown(result, unsafe_allow_html=True))
-    col.image(img)
+    st.markdown(result, unsafe_allow_html=True)
+    st.image(image)
 
